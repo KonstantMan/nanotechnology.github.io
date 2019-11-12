0b62269ad8576c6f1df4ed941ec77f885bea800d
Konstantinos Manolakos <icsd19127@aegean.gr>
2019-11-13 00:43:57 +0200

    change

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
-//Toggle Nav
-burger.addEventListener('click', () => {
-  nav.classlist.toggle('nav-active');
-});
-
-navLinks.forEach((link, index) => {
-link.style.animation='navLinksFade 0.5s ease forwards ${index/7}s';
-console.log(index/7);
-});
-
-
-
-
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle(".nav-active");
+  });
 }
-
 navSlide();
+
