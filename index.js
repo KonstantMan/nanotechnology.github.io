44a6ae46fbd85c8ddcea89c18e38960a9958c1fa
Konstantinos Manolakos <icsd19127@aegean.gr>
2019-11-13 01:43:48 +0200

    margin

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const navSlide = () => {
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
   });
-//Animate Links
-navLinks.forEach((link,index)=>{
-link.getElementsByClassName.animation=`navLinkFade 0.5s ease forwards ${index/7}s`
-});
-
 
 
 }
